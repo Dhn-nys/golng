c90bf57f9fa30cc20f73c1ca1a280f391645568d
デニス <dhenys.1002@gmail.com>
2021-02-03 14:57:15 +0700

    Create 006.Constantv1.go

---
+++ 006.Constant/006.Constantv1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	
+	const firstname string = "Eko"
+	const lastname = "Khannedy"
+	const value = "1000"
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+	fmt.Println(value)
+
+}
