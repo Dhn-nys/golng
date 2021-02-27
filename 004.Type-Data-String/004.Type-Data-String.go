957060274a1e130d9934709eff9d990cd6123b70
デニス <dhenys.1002@gmail.com>
2021-02-28 03:42:21 +0700

    Update and rename 004.String.go to 004.Type-Data-String.go
    Remove "Line 9 - Line 12"

--- 004.Type-Data-String/004.Type-Data-String.go
+++ 004.Type-Data-String/004.Type-Data-String.go
@@ -6,8 +6,4 @@ func main () {
 	fmt.Println("Eko")
 	fmt.Println("Eko Kurniawan")
 	fmt.Println("Eko Kurniawan Khannedy")
-
-	fmt.Println(len("Eko"))
-	fmt.Println("Eko Kurniawan"[0])
-	fmt.Println("Eko Kurniawan Khannedy"[1])
 }
